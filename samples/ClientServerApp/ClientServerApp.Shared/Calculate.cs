8a1d5aebcbbd8784e41e0fda67dbf06dd18230ce
Dainis Silamikelis <dainis.silamikelis@gmail.com>
2017-09-09 15:09:58 +0300

    First

---
+++ samples/ClientServerApp/ClientServerApp.Shared/Calculate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientServerApp.Shared
+{
+    public class Calculate
+    {
+        public int test()
+        {
+            return 26 + 7 * 3;
+        }
+
+    }
+}
